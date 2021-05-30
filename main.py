ab779a976bea089f5cff4d757637487b097cf1bc
Valeria <54101215+uguha@users.noreply.github.com>
2021-05-30 12:30:02 +0300

    create main.py
    v.1

---
+++ main.py
@@ -0,0 +1,82 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.scrollview import ScrollView
+ 
+class ScrButton(Button):
+    def __init__(self, screen, direction='right', goal='main', **kwargs):
+        super().__init__(**kwargs)
+        self.screen = screen
+        self.direction = direction
+        self.goal = goal
+    def on_press(self):
+        self.screen.manager.transition.direction = self.direction
+        self.screen.manager.current = self.goal
+        
+class MainScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+ 
+       vl = BoxLayout(orientation='vertical', padding=8, spacing=8)
+       hl = BoxLayout()
+       txt = Label(text= 'Выбери экран')
+ 
+       vl.add_widget(ScrButton(self, direction='left', goal='first', text="1"))
+       vl.add_widget(ScrButton(self, direction='right', goal='second', text="2"))
+       vl.add_widget(ScrButton(self, direction='down', goal='third', text="3"))
+       vl.add_widget(ScrButton(self, direction='up', goal='fourth', text="4"))
+ 
+       hl.add_widget(txt)
+       hl.add_widget(vl)
+       self.add_widget(hl)
+ 
+class FirstScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+       vl = BoxLayout(orientation='vertical', size_hint=(.5, .5), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+       btn = Button(text= 'Выбор: 1', size_hint=(.5, 1), pos_hint={'left': 0})
+       btn_back = ScrButton(self, direction='right', goal='main', text="Назад", size_hint=(.5, 1), pos_hint={'right': 1})
+       vl.add_widget(btn)
+       vl.add_widget(btn_back)
+       self.add_widget(vl)
+ 
+class SecondScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+ 
+       vl = BoxLayout(orientation='vertical')
+ 
+       hl_0 = BoxLayout(size_hint=(0.8, None), height='30sp')
+       hl_0.add_widget(Label(text='Введите пароль:', halign='right'))
+       self.input = TextInput(multiline=False)
+       hl_0.add_widget(self.input)
+       vl.add_widget(hl_0)
+ 
+       hl = BoxLayout()
+       self.txt = Label(text= 'Выбор: 2')  
+       btn_false = Button(text="OK!")
+       btn_back = ScrButton(self, direction='left', goal='main', text="Назад")
+       vl.add_widget(self.txt)
+       hl.add_widget(btn_false)
+       hl.add_widget(btn_back)
+       vl.add_widget(hl)
+       self.add_widget(vl)
+       btn_false.on_press = self.change_text
+ 
+   def change_text(self):
+       self.txt.text = self.input.text + '? Не сработало ...'    
+ 
+ 
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainScr(name='main'))
+        sm.add_widget(FirstScr(name='first'))
+        sm.add_widget(SecondScr(name='second'))
+        return sm
+ 
+app = MyApp()
+app.run()
